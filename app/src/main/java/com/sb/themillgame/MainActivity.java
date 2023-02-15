5d973052f87392f688de67d44f706f98357084f6
sami <samidare@tutanota.com>
2023-02-15 21:19:31 +0100

    players turn, setting of the graph of the board

--- app/src/main/java/com/sb/themillgame/MainActivity.java
+++ app/src/main/java/com/sb/themillgame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sb.themillgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
@@ -8,39 +9,308 @@ import android.view.View;
 import android.widget.Button;
 
 import com.sb.themillgame.databinding.ActivityMainBinding;
+import com.sb.themillgame.model.Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+    private static int[] idArray = {R.id.touch_piece1, R.id.touch_piece2, R.id.touch_piece3, R.id.touch_piece4, R.id.touch_piece5, R.id.touch_piece6, R.id.touch_piece7, R.id.touch_piece8, R.id.touch_piece8, R.id.touch_piece9, R.id.touch_piece10, R.id.touch_piece11, R.id.touch_piece12, R.id.touch_piece13, R.id.touch_piece14, R.id.touch_piece15, R.id.touch_piece16, R.id.touch_piece17, R.id.touch_piece18, R.id.touch_piece19, R.id.touch_piece20, R.id.touch_piece21, R.id.touch_piece22, R.id.touch_piece23, R.id.touch_piece24};
+
+    private AppCompatButton[]intersections = new AppCompatButton[idArray.length];
+
+    private static int playerTurn = 1 ;
+
+    private Graph board = new Graph();
     private ActivityMainBinding binding ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
-        binding.touchPiece1.setOnClickListener(view -> {
-            setPiece(view, binding.touchPiece1);
-        });
 
-        binding.touchPiece2.setOnClickListener(view -> setPiece(view, binding.touchPiece2));
-        binding.touchPiece3.setOnClickListener(view -> setPiece(view, binding.touchPiece3));
-        binding.touchPiece4.setOnClickListener(view->setPiece(view, binding.touchPiece4));
-        binding.touchPiece5.setOnClickListener(view -> setPiece(view, binding.touchPiece5));
-        binding.touchPiece6.setOnClickListener(view -> setPiece(view, binding.touchPiece6));
-        binding.touchPiece7.setOnClickListener(view -> setPiece(view, binding.touchPiece7));
-        binding.touchPiece8.setOnClickListener(view->setPiece(view, binding.touchPiece8));
-        binding.touchPiece9.setOnClickListener(view -> setPiece(view, binding.touchPiece9));
-        binding.touchPiece10.setOnClickListener(view -> setPiece(view, binding.touchPiece10));
-        binding.touchPiece11.setOnClickListener(view -> setPiece(view, binding.touchPiece11));
-        binding.touchPiece12.setOnClickListener(view->setPiece(view, binding.touchPiece12));
-        binding.touchPiece13.setOnClickListener(view -> setPiece(view, binding.touchPiece13));
-        binding.touchPiece14.setOnClickListener(view -> setPiece(view, binding.touchPiece14));
-        binding.touchPiece15.setOnClickListener(view -> setPiece(view, binding.touchPiece15));
-        binding.touchPiece16.setOnClickListener(view->setPiece(view, binding.touchPiece16));
-        binding.touchPiece17.setOnClickListener(view -> setPiece(view, binding.touchPiece17));
+        setBoard();
+
+        // Une classe MainActivity ou on initialise le board et les 3 phases du jeu
+        // Une classe pour chaque phase
+        // Une classe abstraite IA
+        // 3 sous classes de IA pour chaque niveau
+        // (1 classe joueur ou piece) à voir
+        for (int i = 0 ; i<intersections.length; i++){
+            intersections[i] = findViewById(idArray[i]);
+            final int selectedIntersection = i ;
+            intersections[i].setOnClickListener(view -> {
+                setPiece((AppCompatButton) view, selectedIntersection);
+            });
+        }
+
+
+
 
     }
 
-    private void setPiece(View view, Button b){
+    public void setBoard() {
+        //1
+        addIntersection(0,1);
+        addIntersection(0,7);
 
-        b.setBackground(getDrawable(R.drawable.white_button));
+        //2
+        addIntersection(1,2);
+        addIntersection(1,9);
+
+        //3
+        addIntersection(2,3);
+
+        //4
+        addIntersection(3,11);
+        addIntersection(3,4);
+
+        //5
+        addIntersection(4,5);
+
+        //6
+        addIntersection(5,13);
+        addIntersection(5,6);
+
+        //7
+        addIntersection(6,7);
+
+        //8
+        addIntersection(7,15);
+
+        //9
+        addIntersection(8,9);
+        addIntersection(8,15);
+
+        //10
+        addIntersection(9,10);
+        addIntersection(9,17);
+
+        //11
+        addIntersection(10,11);
+
+        //12
+
+        addIntersection(11,19);
+        addIntersection(11,12);
+
+        //13
+        addIntersection(12,13);
+
+
+        //14
+        addIntersection(13,21);
+        addIntersection(13,14);
+
+
+        //15
+        addIntersection(14,15);
+
+
+        //16
+        addIntersection(15,23);
+
+
+        //17
+        addIntersection(16,17);
+        addIntersection(16,23);
+
+        //18
+        addIntersection(17,18);
+
+        //19
+        addIntersection(18,19);
+
+        //20
+        addIntersection(19,20);
+
+        //21
+        addIntersection(20,21);
+
+        //22
+        addIntersection(21,22);
+
+        //23
+        addIntersection(22,23);
+
+    }
+    public void addIntersection(int id1, int id2) {
+
+        //maxEdge est le nombre d'Edge maximal que la position peut avoir
+        int maxEdgeId1= getNbEdgeMax(id1);
+        int maxEdgeId2= getNbEdgeMax(id2);
+        if(!(board.hasEdge(id1,id2))) {
+
+            if(board.edgeNumber(id1) < maxEdgeId1 && board.edgeNumber(id2)< maxEdgeId2) {
+                board.addEdge(id1, id2, "1");
+            }
+        }
+    }
+
+    private void setPiece(AppCompatButton b, int pieceNumber){
+        if (isIntersectionSelectable(pieceNumber)){
+            if (playerTurn == 1){
+                this.board.setMatrix(pieceNumber, pieceNumber, "1");
+                b.setBackground(getDrawable(R.drawable.white_piece));
+                playerTurn = 2 ;
+            }
+            else{
+                this.board.setMatrix(pieceNumber, pieceNumber, "2");
+                b.setBackground(getDrawable(R.drawable.black_piece));
+                playerTurn = 1 ;
+            }
+        }
+
+        //b.setBackground(getDrawable(R.drawable.white_button));
+    }
+
+    public boolean isIntersectionSelectable(int pieceID){
+        boolean response = false;
+        if (board.getMatrix()[pieceID][pieceID].equals("0")){
+            response = true ;
+        }
+        return response ;
+    }
+
+    //d'après le dessin du plateau
+    public int getNbEdgeMax(int id) {
+        if((id == 9) || (id == 11) || (id == 13) || (id ==15) ){
+            return 4;
+        }else if(id%2 == 1) {
+            return 3;
+        }else {
+            return 2;
+        }
+    }
+
+
+    //retourne une liste de position dont l'id en parametre peut se replacer
+    public ArrayList<String> canMove(int id) {
+        ArrayList<String>res = new ArrayList<>();
+        ArrayList<String>list = board.getNeighbor(id);
+        for(int i =0; i< list.size();i++) {
+            int tmp = Integer.valueOf(list.get(i));
+            if((board.getMatrix()[tmp][tmp].equals("0"))) {
+                res.add(String.valueOf(tmp));
+            }
+        }
+        return res;
+    }
+
+    //on recupere canMove de id, on regarde si newID est dedant,
+    //si oui: on met à 0 la position id ET on met à 1 la posion newId
+    public boolean movePiece(int id, int newId) {
+        if(!canMove(id).contains(String.valueOf(newId))) {
+            return false;
+        }else {
+            board.setMatrix(id,id,"0");
+            board.setMatrix(newId, newId, "1");
+            return true;
+        }
+    }
+
+    //prend en parametre l'id de la derniere position du pion, retourne une liste
+    //de 4 id
+    public LinkedList<String> getMillCombinations(int lastPositionOfId) {
+        LinkedList<String>res = new LinkedList<>();
+        int tmp1;
+        int tmp2;
+        int tmp3;
+        int tmp4;
+        if(lastPositionOfId % 2 ==1) {
+            if(lastPositionOfId <= 7) {
+                tmp1 = (lastPositionOfId+8);
+                tmp2 = (lastPositionOfId+16);
+                tmp3 = (lastPositionOfId-1);
+                tmp4 = (lastPositionOfId+1)%8;
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+
+            else if(lastPositionOfId <= 15) {
+                tmp1 = ((lastPositionOfId-8));
+                tmp2 = ((lastPositionOfId+8));
+                tmp3 = (lastPositionOfId-1);
+                tmp4 = ((lastPositionOfId+1)%8)+8;
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+            else if(lastPositionOfId <= 23) {
+                tmp1 = (lastPositionOfId-16);
+                tmp2 = (lastPositionOfId-8);
+                tmp3 = (lastPositionOfId-1);
+                tmp4 = ((lastPositionOfId+1)%8)+16;
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+        }else {
+            if(lastPositionOfId <= 7) {
+                tmp1 = Math.floorMod(lastPositionOfId-1,8);
+                tmp2 = Math.floorMod(lastPositionOfId-2,8);
+                tmp3 = (lastPositionOfId+1);
+                tmp4 = (lastPositionOfId+2)%8;
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+
+            else if(lastPositionOfId <= 15) {
+                tmp1 = ((lastPositionOfId+1)%8)+8;
+                tmp2 = ((lastPositionOfId+2)%8)+8;
+                tmp3 = ((lastPositionOfId-1)%8)+8;
+                tmp4 = ((lastPositionOfId-2)%8)+8;
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+            else if(lastPositionOfId <= 23) {
+                if(lastPositionOfId == 16) {
+                    tmp1 = (lastPositionOfId+1);
+                    tmp2 = (lastPositionOfId+2);
+                    tmp3 = ((lastPositionOfId+7));
+                    tmp4 = ((lastPositionOfId+6));
+                }else {
+                    tmp1 = ((lastPositionOfId+1)%16)+16;
+                    if(lastPositionOfId == 22) {
+                        tmp2 = ((lastPositionOfId+1)%16)+9;
+                    }else {
+                        tmp2 = ((lastPositionOfId+2)%16)+16;
+                    }
+                    tmp3 = ((lastPositionOfId-1)%16)+16;
+                    tmp4 = ((lastPositionOfId-2)%16)+16;
+
+                }
+                res.add(String.valueOf(tmp1));
+                res.add(String.valueOf(tmp2));
+                res.add(String.valueOf(tmp3));
+                res.add(String.valueOf(tmp4));
+            }
+
+        }
+        return res;
+    }
+
+    //test si l'id en paremetre possède un alignement
+    public Boolean isMill(int lastPosition){
+        LinkedList<String>list = getMillCombinations(lastPosition);
+
+        int nb1 = Integer.parseInt(list.get(0));
+        int nb2 = Integer.parseInt(list.get(1));
+        int nb3 = Integer.parseInt(list.get(2));
+        int nb4 = Integer.parseInt(list.get(3));
+
+
+        if(board.getMatrix()[nb1][nb1].equals("1") && board.getMatrix()[nb2][nb2].equals("1")) {
+            return true;
+        }else return board.getMatrix()[nb3][nb3].equals("1") && board.getMatrix()[nb4][nb4].equals("1");
+
     }
 }
--- app/src/main/java/com/sb/themillgame/model/Graph.java
+++ app/src/main/java/com/sb/themillgame/model/Graph.java
@@ -1,10 +1,9 @@
 package com.sb.themillgame.model;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class Graph {
-    final int nb = 24;
+    private static final int NUMBERVERTECES = 24;
     String[][]Matrix;
 
     public Graph(String [][] Matrix) {
@@ -13,112 +12,39 @@ public class Graph {
 
     public Graph() {
         Matrix= new String[24][24];
-        for (int i = 0; i < nb; i++) {
-            for (int j = 0; j < nb; j++) {
+        for (int i = 0; i < NUMBERVERTECES; i++) {
+            for (int j = 0; j < NUMBERVERTECES; j++) {
                 String empty = "0";
                 Matrix[i][j] = empty;
             }
         }
     }
 
-    public void board() {
-        //1
-        addEdge(0,1);
-        addEdge(0,7);
 
-        //2
-        addEdge(1,2);
-        addEdge(1,9);
-
-        //3
-        addEdge(2,3);
-
-        //4
-        addEdge(3,11);
-        addEdge(3,4);
-
-        //5
-        addEdge(4,5);
-
-        //6
-        addEdge(5,13);
-        addEdge(5,6);
-
-        //7
-        addEdge(6,7);
-
-        //8
-        addEdge(7,15);
-
-        //9
-        addEdge(8,9);
-        addEdge(8,15);
-
-        //10
-        addEdge(9,10);
-        addEdge(9,17);
-
-        //11
-        addEdge(10,11);
-
-        //12
-
-        addEdge(11,19);
-        addEdge(11,12);
-
-        //13
-        addEdge(12,13);
-
-
-        //14
-        addEdge(13,21);
-        addEdge(13,14);
-
-
-        //15
-        addEdge(14,15);
-
-
-        //16
-        addEdge(15,23);
-
-
-        //17
-        addEdge(16,17);
-        addEdge(16,23);
-
-        //18
-        addEdge(17,18);
-
-        //19
-        addEdge(18,19);
-
-        //20
-        addEdge(19,20);
-
-        //21
-        addEdge(20,21);
-
-        //22
-        addEdge(21,22);
-
-        //23
-        addEdge(22,23);
-
-    }
-
-    public void affiche() {
-        for (int i = 0; i < nb; i++) {
-            for (int j = 0; j < nb; j++) {
+    public void print() {
+        for (int i = 0; i < NUMBERVERTECES; i++) {
+            for (int j = 0; j < NUMBERVERTECES; j++) {
                 System.out.print(Matrix[i][j]+ " ");
             }
             System.out.println();
         }
     }
 
+    public String[][] getMatrix(){
+        return this.Matrix;
+    }
+
+    public void addEdge(int id1, int id2, String value){
+        Matrix[id1][id2] = value ;
+        Matrix[id2][id1] = value;
+    }
+
+    public void setMatrix(int id1, int id2, String value){
+        Matrix[id1][id2] = value ;
+    }
     public ArrayList<String> getNeighbor(int id) {
         ArrayList<String> res = new ArrayList<>();
-        for(int i =0; i<nb;i++) {
+        for(int i = 0; i< NUMBERVERTECES; i++) {
             if(hasEdge(id,i)) {
                 res.add(String.valueOf(i));
             }
@@ -126,26 +52,13 @@ public class Graph {
         return res;
     }
 
-    public void getAfficheDetaille() {
-        for (int i = 0; i < nb; i++) {
-            System.out.println(i +" : " + nbEdge(i) + " : " + getNeighbor(i));
+    public void printDetails() {
+        for (int i = 0; i < NUMBERVERTECES; i++) {
+            System.out.println(i +" : " + edgeNumber(i) + " : " + getNeighbor(i));
         }
     }
 
     //on ajoute une Edge entre deux pions
-    public void addEdge(int id1, int id2) {
-
-        //maxEdge est le nombre d'Edge maximal dont la position peut avoir
-        int maxEdgeId1= getNbEdgeMax(id1);
-        int maxEdgeId2= getNbEdgeMax(id2);
-        if(!hasEdge(id1,id2)) {
-
-            if(nbEdge(id1) < maxEdgeId1 && nbEdge(id2)< maxEdgeId2) {
-                Matrix[id1][id2] ="1";
-                Matrix[id2][id1] ="1";
-            }
-        }
-    }
 
     //si deux positions ont une Edge
     public boolean hasEdge(int id1, int id2) {
@@ -156,10 +69,10 @@ public class Graph {
     }
 
     //Edge total d'un plateau
-    public int nbEdgeTotal() {
+    public int totalEdgeNumber() {
         int res =0;
-        for (int i = 0; i < nb; i++) {
-            for (int j = 0; j < nb; j++) {
+        for (int i = 0; i < NUMBERVERTECES; i++) {
+            for (int j = 0; j < NUMBERVERTECES; j++) {
                 if(hasEdge(i,j)) {
                     res++;
                 }
@@ -169,9 +82,9 @@ public class Graph {
     }
 
     //le nombre d'Edge d'une position
-    public int nbEdge(int id1) {
+    public int edgeNumber(int id1) {
         int res =0;
-        for(int j =0; j< nb;j++) {
+        for(int j = 0; j< NUMBERVERTECES; j++) {
             if(Matrix[id1][j].equals("1") || Matrix[j][id1].equals("1") ) {
                 res++;
             }
@@ -179,155 +92,7 @@ public class Graph {
         return res;
     }
 
-    //d'après le dessin du plateau
-    public int getNbEdgeMax(int id) {
-        if((id == 9) || (id == 11) || (id == 13) || (id ==15) ){
-            return 4;
-        }else if(id%2 == 1) {
-            return 3;
-        }else {
-            return 2;
-        }
-    }
 
-
-    //retourne une liste de position dont l'id en parametre peut se replacer
-    public ArrayList<String> canMove(int id) {
-        ArrayList<String>res = new ArrayList<>();
-        ArrayList<String>list = getNeighbor(id);
-        for(int i =0; i< list.size();i++) {
-            int tmp = Integer.valueOf(list.get(i));
-            if((Matrix[tmp][tmp].equals("0"))) {
-                res.add(String.valueOf(tmp));
-            }
-        }
-        return res;
-    }
-
-    //on recupere canMove de id, on regarde si newID est dedant,
-    //si oui: on met à 0 la position id ET on met à 1 la posion newId
-    public boolean movePiece(int id, int newId) {
-        if(!canMove(id).contains(String.valueOf(newId))) {
-            return false;
-        }else {
-            Matrix[id][id]="0";
-            Matrix[newId][newId]="1";
-            return true;
-        }
-    }
-
-    public void setPiece(int id) {
-        Matrix[id][id]="1";
-    }
-
-    //prend en parametre l'id de la derniere position du pion, retourne une liste
-    //de 4 id
-    public LinkedList<String> getListForAlignement(int lastPositionOfId) {
-        LinkedList<String>res = new LinkedList<>();
-        int tmp1;
-        int tmp2;
-        int tmp3;
-        int tmp4;
-        if(lastPositionOfId % 2 ==1) {
-            if(lastPositionOfId <= 7) {
-                tmp1 = (lastPositionOfId+8);
-                tmp2 = (lastPositionOfId+16);
-                tmp3 = (lastPositionOfId-1);
-                tmp4 = (lastPositionOfId+1)%8;
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-
-            else if(lastPositionOfId <= 15) {
-                tmp1 = ((lastPositionOfId-8));
-                tmp2 = ((lastPositionOfId+8));
-                tmp3 = (lastPositionOfId-1);
-                tmp4 = ((lastPositionOfId+1)%8)+8;
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-            else if(lastPositionOfId <= 23) {
-                tmp1 = (lastPositionOfId-16);
-                tmp2 = (lastPositionOfId-8);
-                tmp3 = (lastPositionOfId-1);
-                tmp4 = ((lastPositionOfId+1)%8)+16;
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-        }else {
-            if(lastPositionOfId <= 7) {
-                tmp1 = Math.floorMod(lastPositionOfId-1,8);
-                tmp2 = Math.floorMod(lastPositionOfId-2,8);
-                tmp3 = (lastPositionOfId+1);
-                tmp4 = (lastPositionOfId+2)%8;
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-
-            else if(lastPositionOfId <= 15) {
-                tmp1 = ((lastPositionOfId+1)%8)+8;
-                tmp2 = ((lastPositionOfId+2)%8)+8;
-                tmp3 = ((lastPositionOfId-1)%8)+8;
-                tmp4 = ((lastPositionOfId-2)%8)+8;
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-            else if(lastPositionOfId <= 23) {
-                if(lastPositionOfId == 16) {
-                    tmp1 = (lastPositionOfId+1);
-                    tmp2 = (lastPositionOfId+2);
-                    tmp3 = ((lastPositionOfId+7));
-                    tmp4 = ((lastPositionOfId+6));
-                }else {
-                    tmp1 = ((lastPositionOfId+1)%16)+16;
-                    if(lastPositionOfId == 22) {
-                        tmp2 = ((lastPositionOfId+1)%16)+9;
-                    }else {
-                        tmp2 = ((lastPositionOfId+2)%16)+16;
-                    }
-                    tmp3 = ((lastPositionOfId-1)%16)+16;
-                    tmp4 = ((lastPositionOfId-2)%16)+16;
-
-                }
-                res.add(String.valueOf(tmp1));
-                res.add(String.valueOf(tmp2));
-                res.add(String.valueOf(tmp3));
-                res.add(String.valueOf(tmp4));
-            }
-
-        }
-        return res;
-    }
-
-    //test si l'id en paremetre possède un alignement
-    public Boolean isAlignement(int lastPosition){
-        LinkedList<String>list = getListForAlignement(lastPosition);
-
-        int nb1 = Integer.valueOf(list.get(0));
-        int nb2 = Integer.valueOf(list.get(1));
-        int nb3 = Integer.valueOf(list.get(2));
-        int nb4 = Integer.valueOf(list.get(3));
-
-
-        if(Matrix[nb1][nb1].equals("1") && Matrix[nb2][nb2].equals("1")) {
-            return true;
-        }else if(Matrix[nb3][nb3].equals("1") && Matrix[nb4][nb4].equals("1")) {
-            return true;
-        }else {
-            return false;
-        }
-
-    }
 
 }
 
